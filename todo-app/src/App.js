9e190de7e95de3b620c2724ebda8f996da8cce85
mizk62 <mizuki.umeda@finc.com>
2019-11-13 16:42:39 +0900

    test

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactaaa
         </a>
       </header>
     </div>
